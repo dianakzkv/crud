ef1e947a400ed99aa749f10c69187314e4916ca3
Diana <dianushka.di.jesse@gmail.com>
2023-10-13 14:49:27 +0300

    1

--- src/route/index.js
+++ src/route/index.js
@@ -4,126 +4,22 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
-
-class User {
-  static #list = []
-
-  constructor(email, login, password) {
-    this.email = email
-    this.login = login
-    this.password = password
-    this.id = new Date().getTime()
-  }
-
-  verifyPassword = (password) => this.password === password
-
-  static add = (user) => {
-    this.#list.push(user)
-  }
-
-  static getList = () => this.#list
-
-  static getById = (id) =>
-    this.#list.find((user) => user.id === id)
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (user) => user.id === id,
-    )
-
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true
-    } else {
-      return false
-    }
-  }
-
-  static updateById = (id, { email }) => {
-    const user = this.getById(id)
-
-    if (user) {
-      this.update(user, data)
-      return true
-    } else {
-      return false
-    }
-  }
-
-  static update = (user, { email }) => {
-    if (email) {
-      user.email = email
-    }
-  }
-}
-
+class Purchase {}
 // ================================================================
 
-// router.get Створює нам один ентпоїнт
-
-// ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const list = User.getList()
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
-
+  res.render('purchase-index', {
+    style: 'purchase-index',
     data: {
-      users: {
-        list,
-        isEmpty: list.length === 0,
-      },
+      img: 'https://picsum.photos/250/300',
+      title: 'MI xciomi',
+      description: 'Intel Core i7 / SSD 1TB / RAM 48 GB',
+      category: [
+        { id: 1, text: 'Ready to delivery' },
+        { id: 2, text: 'TOP' },
+      ],
+      pric: 32000,
     },
   })
 })
-// ↑↑ сюди вводимо JSON дані
-
-// ================================================================
-
-router.post('/user-create', function (req, res) {
-  const { email, login, password } = req.body
-  const user = new User(email, login, password)
-  User.add(user)
-  console.log(User.getList())
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'User created',
-  })
-})
-
-// ================================================================
-
-router.get('/user-delete', function (req, res) {
-  const { id } = req.query
-  console.log(id)
-
-  User.deleteById(Number(id))
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'User deleted',
-  })
-})
-
-// ================================================================
-
-router.post('/user-update', function (req, res) {
-  const { email, password, id } = req.body
-  const user = User.getById(Number(id))
-  let result = false
-  if (user.verifyPassword(password)) {
-    User.update(user, { email })
-    result = true
-  }
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: result ? 'Email updated' : 'Error...',
-  })
-})
-
-// Підключаємо роутер до бек-енду
 module.exports = router
