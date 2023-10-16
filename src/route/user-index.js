5c3bcf56cc5d75e26b834818d4a397479c20c14f
Diana <dianushka.di.jesse@gmail.com>
2023-10-16 16:06:09 +0300

    start spotify practice

---
+++ src/route/user-index.js
@@ -0,0 +1,129 @@
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
+
+// ================================================================
+
+class User {
+  static #list = []
+
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
+
+  verifyPassword = (password) => this.password === password
+
+  static add = (user) => {
+    this.#list.push(user)
+  }
+
+  static getList = () => this.#list
+
+  static getById = (id) =>
+    this.#list.find((user) => user.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (user) => user.id === id,
+    )
+
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, { email }) => {
+    const user = this.getById(id)
+
+    if (user) {
+      this.update(user, data)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (user, { email }) => {
+    if (email) {
+      user.email = email
+    }
+  }
+}
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const list = User.getList()
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('index', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'index',
+
+    data: {
+      users: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+})
+// ↑↑ сюди вводимо JSON дані
+
+// ================================================================
+
+router.post('/user-create', function (req, res) {
+  const { email, login, password } = req.body
+  const user = new User(email, login, password)
+  User.add(user)
+  console.log(User.getList())
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'User created',
+  })
+})
+
+// ================================================================
+
+router.get('/user-delete', function (req, res) {
+  const { id } = req.query
+  console.log(id)
+
+  User.deleteById(Number(id))
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'User deleted',
+  })
+})
+
+// ================================================================
+
+router.post('/user-update', function (req, res) {
+  const { email, password, id } = req.body
+  const user = User.getById(Number(id))
+  let result = false
+  if (user.verifyPassword(password)) {
+    User.update(user, { email })
+    result = true
+  }
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: result ? 'Email updated' : 'Error...',
+  })
+})
+
+// Підключаємо роутер до бек-енду
+module.exports = router
